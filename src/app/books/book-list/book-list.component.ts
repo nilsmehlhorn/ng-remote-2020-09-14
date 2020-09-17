08443e33b17b785500034adcac109a6dc0d6614a
mehlhorn <mail@nils-mehlhorn.de>
2020-09-17 14:38:09 +0200

    Lifcecyle

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BookDataService } from '../services/book-data.service';
 import { Book } from '../models/book';
+import { Subject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss'],
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
   books: Book[];
+
+  destroy = new Subject<void>();
 
   constructor(private bookData: BookDataService) {}
 
   ngOnInit(): void {
-    this.bookData.getBooks().subscribe((books: Book[]) => {
-      this.books = books;
-    });
+    this.bookData
+      .getBooks()
+      .pipe(takeUntil(this.destroy))
+      .subscribe((books: Book[]) => {
+        this.books = books;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
   }
 }
