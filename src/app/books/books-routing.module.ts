9d21c606a0e7c2b302263812ef747102b12a5470
mehlhorn <mail@nils-mehlhorn.de>
2020-09-18 16:04:49 +0200

    Guard

--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookDetailComponent } from './book-detail/book-detail.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BooksComponent } from './books/books.component';
+import { ConfirmDeactivateGuard } from '../guards/confirm-deactivate.guard'
 
 const routes: Routes = [
   {
@@ -16,6 +17,7 @@ const routes: Routes = [
       {
         path: ':isbn',
         component: BookDetailComponent,
+        canDeactivate: [ConfirmDeactivateGuard]
       },
     ],
   },
---
+++ src/app/guards/confirm-deactivate.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanDeactivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { BookDetailComponent } from '../books/book-detail/book-detail.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfirmDeactivateGuard
+  implements CanDeactivate<BookDetailComponent> {
+  canDeactivate(
+    component: BookDetailComponent,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return confirm('Leave?');
+  }
+}
